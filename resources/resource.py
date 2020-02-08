80a03070ee0b86c0e1bb8fca00b1ae7c2be3b912
Suchismita <sp6418@imperial.ac.uk>
2020-02-08 15:00:46 +0000

    created new branch called 'resources'. created resources directory. created resource.py and wikipedia.py to store resource superclass and wikipedia subclass. written headers for lookup function

---
+++ resources/resource.py
@@ -0,0 +1,11 @@
+class Resource:
+    def __init__(url, description, author)__:
+        self.url = url
+        self.description = description
+        self.author = author
+        
+    
+    def lookup(self, keyword):
+        pass  
+    
+    
---
+++ resources/wikipedia.py
@@ -0,0 +1,9 @@
+import Resource
+
+class Wikipedia(Resource):
+    def __init__(url, description, author)__:
+        super().__init__(url, description, author) 
+        
+    def lookup(keywords):
+        pass
+                
