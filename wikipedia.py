3de10370e4aa89780d89d832dcff1e7856ffb735
Giacomo La Scala <gl818@ic.ac.uk>
2020-02-08 22:15:37 +0000

    Wikipedia resource manager

--- wikipedia.py
+++ wikipedia.py
@@ -1,9 +1,28 @@
+import requests
+
 from suggestion import Suggestion
 from typing import List
 from resource import Resource
 
 
 class Wikipedia(Resource):
-    def lookup(self, keyword: str) -> List[Suggestion]:
-        return super().lookup(keyword)
+    URL = 'https://en.wikipedia.org/w/api.php'
+    limit = 3
+    parameters = { 'action': 'opensearch', 'format': 'json', 'limit' : str(limit) }
 
+    def lookup(self, keyword: str) -> List[Suggestion]:
+        self.parameters['search'] = keyword
+        search = requests.Session()
+        response = search.get(url=self.URL, params=self.parameters)
+        results = response.json()
+        urls = results[1]
+        titles = results[3]
+        suggestions = []
+        i = 0
+        for _ in urls:
+            if i >= self.limit:
+                break
+            suggestion = Suggestion(titles[i], urls[i], None)
+            suggestions.append(suggestion)
+            i += 1
+        return suggestions
