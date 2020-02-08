2ead919649d72f5106c1fb94f23213a1ac609c7d
Suchismita <sp6418@imperial.ac.uk>
2020-02-08 15:17:10 +0000

    changed resources.py to include suggestions and resources class

--- resources/resource.py
+++ resources/resource.py
@@ -1,11 +1,11 @@
-class Resource:
+class Suggestion:
     def __init__(url, description, author)__:
         self.url = url
         self.description = description
         self.author = author
         
-    
-    def lookup(self, keyword):
-        pass  
+class Resource:
+    def lookup(keyword):
+        pass
     
     
