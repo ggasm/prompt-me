fafe383161921981bc327923fe13194808800e43
suchismitapaul <59656996+suchismitapaul@users.noreply.github.com>
2020-02-09 01:37:45 +0000

    Created books.py

---
+++ books.py
@@ -0,0 +1,43 @@
+from typing import List
+
+import requests
+
+from resource import Resource
+from suggestion import Suggestion
+
+
+class BooksApi(Resource):
+
+    def lookup(self, keyword: str, maxResults: int = 3) -> List[Suggestion]:
+
+        # not printing more than 1 result
+        parameters = {
+            'q': keyword,
+            'langRestrict': 'en',
+            'orderBy': 'relevance',
+            'access_token': 'oauth2-token',
+        }
+
+        get_response = requests.get('https://www.googleapis.com/books/v1/volumes?', parameters)
+        responses = get_response.json()
+
+        suggestions = []
+        i = 0;
+        for response in responses['items']:
+            authors_suggestion: str = ""
+            authors = response['volumeInfo']['authors']
+
+            for author in authors:
+                authors_suggestion = authors_suggestion + authors if (
+                            authors_suggestion == '') else authors_suggestion + ', ' + author
+
+            if authors_suggestion == "":
+                authors_suggestion = 'no authors found'
+
+            suggestions.append(Suggestion((response['volumeInfo']).get("previewLink", 0),
+                                          (response['volumeInfo']).get("title", 0) + ': ' + (
+                                              response['volumeInfo']).get(
+                                              "title", 0),
+                                          authors_suggestion))
+            i += 1
+        return suggestions
